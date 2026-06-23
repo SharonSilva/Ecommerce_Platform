0396ea89b54dfc78bbbd39ae93baf44a1362de98
Andrea Sharon Silva <sharonandrea245@gmail.com>
2026-06-23 13:29:54 +0530

    Fetch categories and drop in the filters

--- Ecommerce.Api/obj/Debug/net10.0/Ecommerce.Api.AssemblyInfo.cs
+++ Ecommerce.Api/obj/Debug/net10.0/Ecommerce.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba32a1f26cd70ef88cada49000393216e380ac2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0b9c8422484a585a546f7db712ecfdc0f2bfa8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
